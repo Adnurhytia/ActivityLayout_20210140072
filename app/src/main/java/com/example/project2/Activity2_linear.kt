58d3ac2a73751d3b95fa29a61a5d65d1a204aea7
Adnurhytia <adindanurhayati30@gmail.com>
2023-10-05 00:38:39 +0700

    Activity 2

---
+++ app/src/main/java/com/example/project2/Activity2_linear.kt
@@ -0,0 +1,11 @@
+package com.example.project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2_linear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity2_linear)
+    }
+}
